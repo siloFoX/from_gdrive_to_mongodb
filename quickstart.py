a35a2d0cb851d2cfdedbb2f6dab9b57e54a984ac
siloFOX <ladnav0704@gmail.com>
2019-08-19 16:53:23 +0900

    fix useless code

--- quickstart.py
+++ quickstart.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pymongo import MongoClient
 import csv
 import openpyxl
+import json
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -62,12 +63,11 @@ def insertData(reader, collection):
                               {'$set' : list},
                               upsert=True) 
 
+
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
     """
-
-    #여기부터
 
     service = build('drive', 'v3', credentials=auth())
 
@@ -88,7 +88,7 @@ def main():
             xlsx = pd.ExcelFile(cwd)
             for sheet in xlsx.sheet_names :
                 PC = xlsx.parse(sheet)
-                PC.to_csv(item['name'] + '_' + sheet + '.csv', encoding='utf-8', index=False)
+                PC.to_csv(sheet + '.csv', encoding='utf-8', index=False)
 
             os.remove(cwd)
 
@@ -101,104 +101,33 @@ def main():
     # db 객체 할당
     db = client['SmartProcess']
 
+    with open('store_image/collection_allocate.json', encoding = 'UTF-8') as f :
+        collections = json.load(f)
+
+    collection_list = []
+    idx_dict = dict()
+
     #collection 객체 할당받기
-    Sample_preparation = db.Sample_preparation
-    Washing1=db.Washing1
-    Washing2=db.Washing2
-    Washing3=db.Washing3
-    Cleaning_blowing=db.Cleaning_blowing
-    Pre_sputtering=db.Pre_sputtering
-    Sputtering=db.Sputtering
-    Heat_treatment=db.Heat_treatment
-    PR_hand_blowing=db.PR_hand_blowing
-    HMDS_coating=db.HMDS_coating
-    PR_coating=db.PR_coating
-    PR_baking=db.PR_baking
-    PR_cooling=db.PR_cooling
-    Chlorobenzene_treatment=db.Chlorobenzene_treatment
-    DI_cleaning=db.DI_cleaning
-    Blowing_after_chlorobenzene_cleaning=db.Blowing_after_chlorobenzene_cleaning
-    Stepper_exposure=db.Stepper_exposure
-    Develop=db.Develop
-    Cleaning_after_develop=db.Cleaning_after_develop
-    Blowing_after_develop_cleaning=db.Blowing_after_develop_cleaning
-    Sample_preparation_for_evaporation=db.Sample_preparation_for_evaporation
-    evaporation=db.evaporation
-    Strip_soaking=db.Strip_soaking
-    Strip_spreading=db.Strip_spreading
-    Oxide_removal=db.Oxide_removal
-    Applying_silver_paste=db.Applying_silver_paste
-    Dry_silver_paste=db.Dry_silver_paste
-    Measure=db.Measure
+    for idx, val in enumerate(collections.values()) :
+        collection_list.append(db.get_collection(val))
+        idx_dict[val] = idx
 
     folder = os.getcwd()
-    for filename in os.listdir(folder):
+
+    for filename in os.listdir(folder) :
+
         fullname = os.path.join(folder, filename)
-        if fullname.find('.csv') is not -1:
+
+        if fullname.find('.csv') + 1 :
+
             csvfile = open(fullname, 'rt', encoding='utf-8')
             reader = csv.DictReader(csvfile)
-            if filename.find('샘플준비.csv') is not -1:
-                insertData(reader, Sample_preparation)
-            elif filename.find('세정1.csv') is not -1:
-                insertData(reader, Washing1)
-            elif filename.find('세정2.csv') is not -1:
-                insertData(reader, Washing2)
-            elif filename.find('세정3.csv') is not -1:
-                insertData(reader, Washing3)
-            elif filename.find('세정블로윙.csv') is not -1:
-                insertData(reader, Cleaning_blowing)
-            elif filename.find('프리스퍼터링.csv') is not -1:
-                insertData(reader, Pre_sputtering)
-            elif filename.find('스퍼터링.csv') is not -1:
-                insertData(reader, Sputtering)
-            elif filename.find('열처리.csv') is not -1:
-                insertData(reader, Heat_treatment)
-            elif filename.find('PR핸드블로윙.csv') is not -1:
-                insertData(reader, PR_hand_blowing)
-            elif filename.find('HMDS코팅.csv') is not -1:
-                insertData(reader, HMDS_coating)
-            elif filename.find('PR코팅.csv') is not -1:
-                insertData(reader, PR_coating)
-            elif filename.find('PR베이킹.csv') is not -1:
-                insertData(reader, PR_baking)
-            elif filename.find('PR쿨링.csv') is not -1:
-                insertData(reader, PR_cooling)
-            elif filename.find('클로로벤젠처리.csv') is not -1:
-                insertData(reader, Chlorobenzene_treatment)
-            elif filename.find('클로로벤젠세정후블로윙.csv') is not -1:
-                insertData(reader, Blowing_after_chlorobenzene_cleaning)
-            elif filename.find('노광.csv') is not -1:
-                insertData(reader, Stepper_exposure)
-            elif filename.find('현상.csv') is not -1:
-                insertData(reader, Develop)
-            elif filename.find('현상후세정.csv') is not -1:
-                insertData(reader, Cleaning_after_develop)
-            elif filename.find('현상액세정후블로윙.csv') is not -1:
-                insertData(reader, Blowing_after_develop_cleaning)
-            elif filename.find('이베포레이션샘플거치.csv') is not -1:
-                insertData(reader, Sample_preparation_for_evaporation)
-            elif filename.find('이베포레이션.csv') is not -1:
-                insertData(reader, evaporation)
-            elif filename.find('스트립-담그기.csv') is not -1:
-                insertData(reader, Strip_soaking)
-            elif filename.find('스트립-뿌리기.csv') is not -1:
-                insertData(reader, Strip_spreading)
-            elif filename.find('산화막제거.csv') is not -1:
-                insertData(reader, Oxide_removal)
-            elif filename.find('실버페이스트도포.csv') is not -1:
-                insertData(reader, Applying_silver_paste)
-            elif filename.find('실버페이스트건조.csv') is not -1:
-                insertData(reader, Dry_silver_paste)
-            elif filename.find('측정.csv') is not -1:
-                insertData(reader, Measure)
+
+            insertData(reader, collection_list[idx_dict[collections[filename.split('.')[0]]]])
+
             csvfile.close()
             os.remove(fullname)
 
 
-
-
-
 if __name__ == '__main__':
     main()
-
-
