746717a99a42056210a30c4846a5a7914ddbe456
raspberry <raspberry6523@gmail.com>
2020-01-06 21:39:22 +0900

    fix : clean version, but will make If not xlsx file exist

--- quickstart.py
+++ quickstart.py
@@ -64,7 +64,7 @@ def insertData(reader, collection):
                               upsert=True) 
 
 def save_2_DB (db) :
-    with open('store_image/collection_allocate.json', encoding = 'UTF-8') as f :
+    with open('collection_allocate.json', encoding = 'UTF-8') as f :
         collections = json.load(f)
 
     collection_list = []
